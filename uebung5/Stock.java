07b12c7c48b7d811cc24a240dd3c9400c25bfdc4
Mansieu <lucie.bechtold@orange.fr>
2014-12-02 15:56:23 +0100

    Add rest of the class (not finished)

--- uebung5/Stock.java
+++ uebung5/Stock.java
@@ -7,29 +7,105 @@ public class Stock
     int[] table;
     int N;
     
+     /**
+    * Method newArticle
+    * 
+    * creates a new article
+    */
     public void newArticle ()
     {
         this.table[this.N] = new Article();
         this.N++;
     }
     
-    
      /**
-    * Method solveLinearEquality
+    * Method deleteArticle
     *
-    * finds x where ax + b = 0,
-    * a,b are given reals
+    * deletes an article
     *
-    * @param a (double), b (double)
-    * @return (String) solution of a linear equality 
+    * @param i (int)
     */
-    public void deleteArticle (int i)
+    public void deleteArticle (int articleNumber)
     {
+        int i = this.getArticleByNumber(articleNumber);
         for (int j=i ; j<this.N ; j++)
         {
             this.table[j] = this.table [j+1];
         }
         this.N--;
+    }
+    
+	/**
+    * Method reduceStock
+    * 
+	* reduces the stock of a certain amount
+	* 
+	* @param  amount (int)
+	*/
+	public void reduceStock (int amount)
+	{
+        check (amount>0 , "Needs to be positive");
+		this.stock = this.stock + amount;
+	}
+    
+	/**
+    * Method augmentStock
+    * 
+	* augments the stock of a certain amount
+	* 
+	* @param  amount (int)
+	*/
+	public void augmentStock (int amount) 
+	{
+        check (amount>0 , "Needs to be positive");
+        check (amount<=stock , "Impossible to deduce more than available");
+		this.stock = this.stock - amount;
+	}
+    
+     /**
+    * Method augmentPrice
+    * 
+	* augments the price of a certain percentage
+	* 
+	* @param  percentage (byte)
+	*/
+    public void augmentPrice (byte percentage)
+    {
+        percentage /= 100;
+        price += price*percentage;
+    }
+    
+     /**
+    * Method reducePrice
+    * 
+	* reduces the price of a certain percentage
+	* 
+	* @param  percentage (byte)
+	*/
+    public void reducePrice (byte percentage)
+    {
+        percentage /= 100;
+        price -= price*percentage;
+    }
+    
+     /**
+    * Method getArticleByNumber
+    * 
+	* finds the rank of the article in the table with its number
+	* 
+	* @param  articleNumber (int)
+    * @return i (int) the rank of the article with the asked number
+	*/
+    public getArticleByNumber (int articleNumber)
+    {
+        for (int i=0; i<N ; i++)
+        {
+            if (articleNumber == table[i].getNumber())
+            {
+                return i;
+            }
+        }
+        return -1;
     }
     
      /**
