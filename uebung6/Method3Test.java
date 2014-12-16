c7654c4e4a46de58a9897d96287220e0619b7771
Walle Cyril <capocyril@hotmail.com>
2014-12-16 17:03:59 +0100

    more tests

---
+++ uebung6/Method3Test.java
@@ -0,0 +1,50 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class Method3Test.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class Method3Test
+{
+    /**
+     * Default constructor for test class Method3Test
+     */
+    public Method3Test()
+    {
+    }
+
+    @Test
+    public void insertionSortTest()
+    {
+        int[] unsorted = {99,3,2,1,45};
+        int[] wanted = {1,2,3,45,99};
+        assertArrayEquals(Method3.insertionSort(unsorted), wanted ,"");
+    }
+    
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
