e4aa8328bbcf3fa34141fd5c5d4e939058463580
Walle Cyril <capocyril@hotmail.com>
2015-02-20 18:40:49 +0100

    Update and rename uebung12 to uebung12/PersonQueueException.java

---
+++ uebung12/PersonQueueException.java
@@ -0,0 +1,9 @@
+public class PersonQueueException extends Exception
+{
+    public PersonQueueException(){}
+    public PersonQueueException(String message)
+    {
+        super(message);
+    }
+
+}
